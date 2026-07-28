a447b13bfe75eae4ba36d9c0bb974540fbc5de6f
Tushalijain <tushalijain1234@gmail.com>
2026-07-28 09:02:16 +0530

    feat: add Docker-based Python execution support

---
+++ backend/src/services/executePythonDocker.js
@@ -0,0 +1,71 @@
+const { spawn } = require("child_process");
+const path = require("path");
+const VERDICTS = require("../constants/verdicts");
+const executePythonDocker = (filePath, input = "") => {
+  return new Promise((resolve, reject) => {
+
+    const fileName = path.basename(filePath);
+
+    const docker = spawn("docker", [
+        "run",
+        "--rm",
+
+        "-i",   
+        
+        "--network",
+        "none",
+
+        "--memory",
+        "128m",
+
+        "--cpus",
+        "1",
+
+        "-v",
+        `${path.dirname(filePath)}:/app`,
+
+        "-w",
+        "/app",
+
+        "python:3.11",
+
+        "python",
+        fileName,
+        ]);
+
+    let output = "";
+    let error = "";
+
+   docker.stdin.write(input + "\n");
+    docker.stdin.end();
+
+    docker.stdout.on("data", (data) => {
+      output += data.toString();
+    });
+
+    docker.stderr.on("data", (data) => {
+      error += data.toString();
+    });
+
+   docker.on("error", (err) => {
+    reject({
+        type: VERDICTS.RUNTIME_ERROR,
+        message: err.message
+    });
+});
+
+docker.on("close", (code) => {
+    if (code === 0) {
+        resolve(output);
+    } else {
+        reject({
+            type: VERDICTS.RUNTIME_ERROR,
+            message: error || "Execution Failed"
+        });
+    }
+});
+
+  });
+};
+
+module.exports = executePythonDocker;
